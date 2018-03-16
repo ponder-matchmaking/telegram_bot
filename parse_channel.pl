bbccc49291c871b52f60eff4c86abd758c6afd61
Chris Imani <chrisvip@users.noreply.github.com>
2018-03-15 23:58:21 -0700

    cleaning up the script

--- parse_channel.pl
+++ parse_channel.pl
@@ -1,1 +1,28 @@
-if (/, (.+?)(:| \[).*>joined /) { $jn{$1}++; print STDERR qq(joined: $1\n); } elsif (/, (.+?)(:| \[).*>invited (.+?)(<| \[)/) { $u1=$1; $u2=$3; if (!$inv{$u2} && !$jn{$u2}) {$inv{$u2}=1; $uinv{$u1}{$u2}=1;} print STDERR qq(invited: $u1 >> $u2\n) } elsif (/^(.*?), (.+?)(:| \[).*(0x[0-9a-fA-F]{40})/) { print STDERR qq(eth: $2 = $4 - $1\n); $eth{$2}=$4; $dt{$2}=$1 } END {foreach $u (keys %eth){ $nv=0; foreach $v (keys(%{$uinv{$u}})){$nv++ if $jn{$v}} %vs=%{$uinv{$u}}; print qq($dt{$u},$u,$eth{$u},).keys(%vs).qq(,$nv\n) } }
+# Script to parse a telegram channel history and export eth addresses and user invites.
+
+if (/, (.+?)(:| \[).*>joined /) { 
+  $jn{$1}++; 
+  print STDERR qq(joined: $1\n); 
+} elsif (/, (.+?)(:| \[).*>invited (.+?)(<| \[)/) { 
+  $u1=$1; $u2=$3; 
+  if (!$inv{$u2} && !$jn{$u2}) {
+    $inv{$u2}=1; 
+    $uinv{$u1}{$u2}=1;
+  } 
+  print STDERR qq(invited: $u1 >> $u2\n) 
+} elsif (/^(.*?), (.+?)(:| \[).*(0x[0-9a-fA-F]{40})/) { 
+  print STDERR qq(eth: $2 = $4 - $1\n); 
+  $eth{$2}=$4; 
+  $dt{$2}=$1;
+} 
+
+END {
+  foreach $u (keys %eth){ 
+    $nv=0;
+    foreach $v (keys(%{$uinv{$u}})){
+      $nv++ if $jn{$v};
+    } 
+    %vs=%{$uinv{$u}}; 
+    print qq($dt{$u},$u,$eth{$u},).keys(%vs).qq(,$nv\n);
+  } 
+}
